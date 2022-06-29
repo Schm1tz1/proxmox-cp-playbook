35aca095bff404c5c8f4362b19a8a5ebb8842356
roman <roman.schmitz>
2022-06-29 23:03:29 +0200

    Fixed template name

--- terraform/preparation/1-create-template.sh
+++ terraform/preparation/1-create-template.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-qm create 9000 --memory 2048 --net0 virtio,bridge=vmbr1 --name CP-Template
+qm create 9000 --memory 2048 --net0 virtio,bridge=vmbr1 --name cp-base-template
 
 # latest supported version: Ubuntu 20
 qm importdisk 9000 /root/img/focal-server-cloudimg-amd64.img local-zfs
@@ -12,7 +12,7 @@ qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-zfs:vm-9000-disk-0
 
 qm set 9000 --ide2 local-zfs:cloudinit
 qm set 9000 --boot c --bootdisk scsi0
-
+Fi
 qm set 9000 --serial0 socket --vga serial0
 
 qm cloudinit dump 9000 user
